89bf1ca77965479277c034f63c986ce6dd665a55
mohamedellithyyyy <ellithy201102@gmail.com>
2025-09-11 19:50:09 +0300

    add/'Starting&ending/'

--- Программирование_Programming'Java'_1_2/Lab1/main.java
+++ Программирование_Programming'Java'_1_2/Lab1/main.java
@@ -1,6 +1,20 @@
-public class Lab1{
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Arrays;
 
+public class Lab1{
+    // Starting and ending for 1D Array№1
+    int arr1_starting = 4;
+    int arr1_ending = 22;
+    // Starting, ending and size for 1D Array№2
+    double arr2_starting = -15.0;
+    double arr2_ending = 15.0;
+    int arr2_size = 18;
+    int array3_col = 10;
+    int array3_row = 18; 
+    public static void main(String[] args) {
+        
     }
+
+    
 }
 
