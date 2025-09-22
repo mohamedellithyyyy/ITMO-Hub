022fdc130bc0f97be237e75d95be56956d4696c4
mohamedellithyyyy <ellithy201102@gmail.com>
2025-09-23 01:53:03 +0300

    Final version of Lab1.java

--- Programming/Labs/Lab1/src/Lab1.java
+++ Programming/Labs/Lab1/src/Lab1.java
@@ -2,72 +2,78 @@ import java.util.Random;
 import java.util.Arrays;
 import java.lang.Math;
 
-public class Lab1 {
-    // Starting and ending for 1D Array№1
-    final static int arr1_starting = 4;
-    final static int arr1_ending = 22;
-    // Starting, ending and size for 1D Array№2
-    final static double arr2_starting = -15.0;
-    final static double arr2_ending = 15.0;
-    final static int arr2_size = 18;
-    // Size for 2D Array№3
+public class lab1 {
+    // 1D Array №1
+    final static short wStarting = 4;
+    final static short wEnding = 22;
+    final static int W_SIZE = ((wEnding - wStarting) / 2) + 1; // 10
+
+    // 1D Array №2
+    final static float xStarting = -15;
+    final static float xEnding = 15;
+    final static int X_SIZE = 18;
+
+    // 2D Array №3 => Matrix
     final static int array3_col = 10;
     final static int array3_row = 18;
     final static int DECIMAL = 2;
+
     public static void main(String[] args) {
-        var array1 = makeArray1(arr1_starting, arr1_ending);
-        System.out.println("First array:\n" + Arrays.toString(array1) + "\n");
-        var array2 = makeArray2(arr2_size, arr2_starting, arr2_ending);
-        System.out.println("Second array:\n" + Arrays.toString(array2) + "\n");
-        long[] p = Arrays.stream(array1).asLongStream().toArray();
-        var array3 = makeArray3(array3_col, array3_row, p, array2);
-        System.out.println("Third matrix:");
-        printMatrix(array3, DECIMAL);
+        // Print first Array (w)
+        short[] w = createWArray(wStarting, wEnding);
+        System.out.println("First array:\n" + Arrays.toString(w));
+        System.out.println("#".repeat(40) + "\n" + "✅✅✅ The First array is done\n\n");
+        // Print second Array (x)
+        float[] x = createXArray(X_SIZE, xStarting, xEnding);
+        System.out.println("Second array:\n" + Arrays.toString(x));
+        System.out.println("#".repeat(40) + "\n" + "✅✅✅ The 2nd array is done\n\n");
+        // Print Matrix
+        var matrix = create2DArray(array3_col, array3_row, w, x);
+        System.out.println("Resulting 2D matrix:");
+        printMatrix(matrix, DECIMAL);
     }
-    // Create D1 Array1
-    public static int[] makeArray1(int start, int end) {
-        var size = (end - start) / 2 + 1;
-        var result = new int[size];
+// to create w array 
+    private static short[] createWArray(short start, short end) {
+        var result = new short[W_SIZE];
         var temp = start;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < W_SIZE; i++) {
             result[i] = temp;
             temp += 2;
         }
         return result;
     }
-    // Create D1 Array2 || a random 1D array
-    private static double[] makeArray2(int size, double start, double end) {
-        var result = new double[size];
+// to create x array 
+    private static float[] createXArray(int size, float start, float end) {
+        var result = new float[size];
         var rand = new Random();
         for (int i = 0; i < size; i++) {
-            result[i] = rand.nextDouble() * (end - start) + start;
+            result[i] = (float) (rand.nextDouble() * (end - start) + start);
         }
         return result;
     }
-    // Create D2 Array3
-    private static double[][] makeArray3(int columnsSize, int rowsSize, long[] p, double[] x) {
+//  
+    private static double[][] create2DArray(int columnsSize, int rowsSize, short[] p, float[] x) {
         var result = new double[columnsSize][rowsSize];
         for (int i = 0; i < columnsSize; i++) {
             for (int j = 0; j < rowsSize; j++) {
-                result[i][j] = calculateValue(p[i], x[j]);
+                result[i][j] = calculateElement(p[i], x[j]);
             }
         }
         return result;
     }
-    // Calculate Value
-private static double calculateValue(long p, double x) {
+// to create calculateElement
+    private static double calculateElement(short p, float x) {
     if (p == 18) {
-        double value = Math.pow(Math.atan(Math.pow(x / (3 * Math.E) + 1, 2)),  2 - Math.pow(x, (x + 3) / 4.0) / (Math.cos(Math.tan(x)) - 1) );
+        float value = (float)Math.pow(Math.atan(Math.pow(x / (3 * Math.E) + 1, 2)),  2 - Math.pow(x, (x + 3) / 4.0) / (Math.cos(Math.tan(x)) - 1) );
+        return  value;
     }
     if (p == 4 || p == 12 || p == 14 || p == 20 || p == 22) {
-        return Math.log(Math.pow(Math.PI * (Math.sqrt(Math.abs(x)) + 1),
-            Math.sin(Math.log(Math.abs(x)))));
+        return Math.log(Math.pow(Math.PI * (Math.sqrt(Math.abs(x)) + 1),Math.sin(Math.log(Math.abs(x)))));
     }
-    return Math.log(Math.pow(Math.cos(Math.pow(Math.E, Math.cos(x))),
-         2 * Math.cos(Math.sin(x))));
-}
-    // Print matrix with given decimal places
-private static void printMatrix(double[][] matrix, int decimalPlaces) {
+    return Math.log(Math.pow(Math.cos(Math.pow(Math.E, Math.cos(x))),2 * Math.cos(Math.sin(x))));
+    }
+// Print matrix with given decimal places
+    private static void printMatrix(double[][] matrix, int decimalPlaces) {
         String format = "%." + decimalPlaces + "f ";
         for (double[] row : matrix) {
             for (double value : row) {
