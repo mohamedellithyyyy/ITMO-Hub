8fed51f8b578fcb264ab247e1bbff6fd77ca0825
Lithium <ellithy201102@gmail.com>
2025-11-14 10:15:15 +0300

    Add All

--- OPD/Labs/Lab1/TEST.sh
+++ OPD/Labs/Lab1/TEST.sh
@@ -7,7 +7,7 @@ echo "Default shell: "$SHELL
 MYVAR="Hello"
 echo "1. Dollar Sign: $MYVAR"
 echo "2. Backtick: Today is `date`"
-echo "3. Double Quotes: \$MYVAR World"
+echo "3. Double Quotes: $MYVAR World"
 echo '4. Single Quotes: $MYVAR World'
 echo "5. Backslash: \$MYVAR is not expanded literally"
 echo -e "lab0/excadrill4" | grep ".txt"
---
+++ OPD/Labs/Lab1/scriptttt.sh
@@ -0,0 +1,3 @@
+MyVar="Hello"
+:wq
+
