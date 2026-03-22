ae42a185e896c308ca4fad0b1e4025838fa90ce7
Lithium JR <ellithy201102@gmail.com>
2026-03-22 16:27:17 +0300

    Update

--- Programming/Semester-2/Labs/Lab5/app/src/models/MusicBand.java
+++ Programming/Semester-2/Labs/Lab5/app/src/models/MusicBand.java
@@ -1,13 +1,59 @@
 package models;
 
-public class MusicBand {
-    private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
-    private String name; //Поле не может быть null, Строка не может быть пустой
-    private Coordinates coordinates; //Поле не может быть null
-    private java.time.ZonedDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
-    private long numberOfParticipants; //Значение поля должно быть больше 0
-    private long albumsCount; //Значение поля должно быть больше 0
-    private MusicGenre genre; //Поле может быть null
-    private Person frontMan; //Поле не может быть null
+import java.time.ZonedDateTime;
+public class MusicBand implements Comparable<MusicBand> {
+    private Integer id;
+    private String name;
+    private Coordinates coordinates;
+    private ZonedDateTime creationDate;
+    private long numberOfParticipants;
+    private long albumsCount;
+    private MusicGenre genre;
+    private Person frontMan;
+    public MusicBand(Integer id, String name, Coordinates coordinates,
+                     ZonedDateTime creationDate, long numberOfParticipants,
+                     long albumsCount, MusicGenre genre, Person frontMan) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.numberOfParticipants = numberOfParticipants;
+        this.albumsCount = albumsCount;
+        this.genre = genre;
+        this.frontMan = frontMan;
+    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Coordinates getCoordinates() { return coordinates; }
+    public void setCoordinates(Coordinates coordinates) { this.coordinates = coordinates; }
+    public ZonedDateTime getCreationDate() { return creationDate; }
+    public void setCreationDate(ZonedDateTime creationDate) { this.creationDate = creationDate; }
+    public long getNumberOfParticipants() { return numberOfParticipants; }
+    public void setNumberOfParticipants(long numberOfParticipants) { this.numberOfParticipants = numberOfParticipants; }
+    public long getAlbumsCount() { return albumsCount; }
+    public void setAlbumsCount(long albumsCount) { this.albumsCount = albumsCount; }
+    public MusicGenre getGenre() { return genre; }
+    public void setGenre(MusicGenre genre) { this.genre = genre; }
+    public Person getFrontMan() { return frontMan; }
+    public void setFrontMan(Person frontMan) { this.frontMan = frontMan; }
+    @Override
+    public int compareTo(MusicBand other) {
+        return Long.compare(this.numberOfParticipants, other.numberOfParticipants);
+    }
 
+    @Override
+    public String toString() {
+        return "MusicBand{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                ", coordinates=" + coordinates +
+                ", creationDate=" + creationDate +
+                ", numberOfParticipants=" + numberOfParticipants +
+                ", albumsCount=" + albumsCount +
+                ", genre=" + genre +
+                ", frontMan=" + frontMan +
+                "}";
+    }
 }
--- Programming/Semester-2/Labs/Lab5/app/src/models/Person.java
+++ Programming/Semester-2/Labs/Lab5/app/src/models/Person.java
@@ -3,5 +3,55 @@ package models;
 import java.time.LocalDate;
 
 public class Person {
-    
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private LocalDate birthday ; //Поле не может быть null
+    private String passportID; //Строка не может быть пустой, Значение этого поля должно быть уникальным, Поле не может быть null
+    private Color hairColor; //Поле может быть null
+    private Location location; //Поле может быть null
+
+    public Person(String name, LocalDate birthday, String passportID, Color hairColor, Location location){
+        this.name = name;
+        this.birthday = birthday;
+        this.passportID = passportID;
+        this.hairColor = hairColor;
+        this.location = location;
+    }
+    /* Setters */
+    /** @return Name */
+    public String getName() {return name;}
+    public LocalDate getBirthday(){return birthday;}
+    public String getPassportID(){return passportID;}
+    public Color getHairColor(){return hairColor;}
+    public Location getLocation(){return location;}
+    /* getters */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setPassportID(String passportID) {
+        this.passportID = passportID;
+    }
+
+    public void setHairColor(Color hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", passportID='" + passportID + '\'' +
+                ", hairColor=" + hairColor +
+                ", location=" + location +
+                '}';
+    }
 }
