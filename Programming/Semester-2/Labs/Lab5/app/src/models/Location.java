c5a2efdf990ac9b81d714d20b1950ab04896e7a5
Lithium JR <ellithy201102@gmail.com>
2026-03-22 04:47:45 +0300

    Finished Location

---
+++ Programming/Semester-2/Labs/Lab5/app/src/models/Location.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Location {
+    private Float x; //Поле не может быть null
+    private Long y; //Поле не может быть null
+    private Integer z; //Поле не может быть null
+    /* @return X coordinate */
+    public Float getX(){
+        return x;
+    }
+    public void setX(Float x){
+        this.x = x;
+    }
+    public Long getY(){
+        return y;
+    }
+
+    public void setY(Long y) {
+        this.y = y;
+    }
+    public Integer getZ(){
+        return z;
+    }
+
+    public void setZ(Integer z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
